95f288f19c4afbcb8b4838623d664399e7c47163
Siddharth Singh <siddharth.singh19@outlook.com>
2024-01-17 01:06:17 +0530

    08

--- 08strings.js
+++ 08strings.js
@@ -3,5 +3,5 @@
 let str1 = "HolaSonal"
 let str2 = "HolaSwadha"
 
-let newStr = str1.slice(1) + str2.slice(1)
-console.log(newStr)
+let newString = str1.slice(1) + str2.slice(1)
+console.log(newString)
