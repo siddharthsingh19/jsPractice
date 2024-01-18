6c5630ee4d28e20288e8cb11d47ac919a62449f1
Siddharth Singh <siddharth.singh19@outlook.com>
2024-01-19 01:52:10 +0530

    11 or

--- 11array.js
+++ 11array.js
@@ -1,12 +1,17 @@
 // wap to find the number of even digits in an array of integers.
 
-let arrr = [1,2,3,4,5,6,7,8,9,10]
-let c = 0
-const checking = (arr) => {
-    for (let i = 0; i<arr.length; i++){
-        arr[i] %2 == 0 ? c+=1 : c
-    }
-    console.log(c)
-} 
+let arrr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let c = 0
+// const checking = (arr) => {
+//     for (let i = 0; i<arr.length; i++){
+//         arr[i] %2 == 0 ? c+=1 : c
+//     }
+//     console.log(c)
+// }
 
-checking(arrr)
+// checking(arrr)
+
+// or
+
+const checking = (arr) => arr.filter((num) => num % 2 === 0).length;
+console.log(checking(arrr));
